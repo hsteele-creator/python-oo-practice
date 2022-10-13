0987da22456581dedadce5714996c24ed612b8ed
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-13 14:33:52 -0400

    finished serial.py

--- serial.py
+++ serial.py
@@ -19,4 +19,15 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
-
+    def __init__(self, start=0):
+        self.start = self.next = start
+        
+    def generate(self):
+        """returns start number the first time and adds 1 every call after"""
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        """resets the number for the generate function back to the start number"""
+        self.next = self.start
+        
